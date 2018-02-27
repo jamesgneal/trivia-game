a9734f479e77e95c076868f2da1376da5f2c7082
jamesgneal <nealjg@gmail.com>
2018-02-26 19:11:58 -0500

    basic object setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+// Trivia data object
+var triviaData = {
+    question1: {
+        q1 : "What engineering company designed and built the Lunar Module (the part that actually landed on the moon)?",
+        a1a : "North American Rockwell",
+        a1b : "Grumman",
+        a1c : "Boeing",
+        a1d : "Martin Marieta",
+    },
+    question2: {
+        q2 : "How many crew members were on each Apollo mission?",
+        a2a : "1",
+        a2b : "2",
+        a2c : "3",
+        a2d : "4",
+    },
+    question3: {
+        q3 : "How many missions actually landed on the moon?",
+        a3a : "1",
+        a3b : "3",
+        a3c : "5",
+        a3d : "6",
+    },
+    question4: {
+        q4 : "What was the name of the vehicle used to launch the command, service and lunar modules to the Moon?",
+        a4a : "Treadstone II",
+        a4b : "Falcon Heavy",
+        a4c : "Saturn V",
+        a4d : "Mercury 7",
+    },
+    question5: {
+        q4 : "Who was the second man to set foot on the moon?",
+        a4a : "Neil Armstrong",
+        a4b : "John Glenn",
+        a4c : "Edwin 'Buzz' Aldrin",
+        a4d : "Jim Lovel;",
+    },
+}
+
+
+});
