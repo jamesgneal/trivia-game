ba4804a6e0c08adb08be4610fa2ec6acbadc0bbc
jamesgneal <nealjg@gmail.com>
2018-03-02 14:25:11 -0500

    content dynamically loads from object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,53 @@
 $(document).ready(function() {
 
 // Trivia data object
-var triviaData = {
+var triviaGame = {
     question1: {
-        q1 : "What engineering company designed and built the Lunar Module (the part that actually landed on the moon)?",
-        a1a : "North American Rockwell",
-        a1b : "Grumman",
-        a1c : "Boeing",
-        a1d : "Martin Marieta",
+        q : "What engineering company designed and built the Lunar Module (the part that actually landed on the moon)?",
+        a1 : "North American Rockwell",
+        a2 : "Grumman",
+        a3 : "Boeing",
+        a4 : "Martin Marieta",
     },
     question2: {
-        q2 : "How many crew members were on each Apollo mission?",
-        a2a : "1",
-        a2b : "2",
-        a2c : "3",
-        a2d : "4",
+        q : "How many crew members were on each Apollo mission?",
+        a1 : "1",
+        a2 : "2",
+        a3 : "3",
+        a4 : "4",
     },
     question3: {
-        q3 : "How many missions actually landed on the moon?",
-        a3a : "1",
-        a3b : "3",
-        a3c : "5",
-        a3d : "6",
+        q : "How many missions actually landed on the moon?",
+        a1 : "1",
+        a2 : "3",
+        a3 : "5",
+        a4 : "6",
     },
     question4: {
-        q4 : "What was the name of the vehicle used to launch the command, service and lunar modules to the Moon?",
-        a4a : "Treadstone II",
-        a4b : "Falcon Heavy",
-        a4c : "Saturn V",
-        a4d : "Mercury 7",
+        q : "What was the name of the vehicle used to launch the command, service and lunar modules to the Moon?",
+        a1 : "Treadstone II",
+        a2 : "Falcon Heavy",
+        a3 : "Saturn V",
+        a4 : "Mercury 7",
     },
     question5: {
-        q4 : "Who was the second man to set foot on the moon?",
-        a4a : "Neil Armstrong",
-        a4b : "John Glenn",
-        a4c : "Edwin 'Buzz' Aldrin",
-        a4d : "Jim Lovel;",
+        q : "Who was the second man to set foot on the moon?",
+        a1 : "Neil Armstrong",
+        a2 : "John Glenn",
+        a3 : "Edwin 'Buzz' Aldrin",
+        a4 : "Jim Lovell",
+    },
+    questionWriter : function(question) {
+        $("#question").text(question.q);
+        $("#a1").text(question.a1);
+        $("#a2").text(question.a2);
+        $("#a3").text(question.a3);
+        $("#a4").text(question.a4);
     },
 }
+
+triviaGame.questionWriter(triviaGame.question1);
+console.log("this is working");
 
 
 });
